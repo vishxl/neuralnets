aa58660b03a73280784df528ea4f4bcdd4b730a4
Vishal Singh <vsalsngh@gmail.com>
2018-01-08 09:24:46 +0530

    Create neural-nets.py

---
+++ ANN-in-Keras/neural-nets.py
@@ -0,0 +1,6 @@
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense
+from keras.optimizers import Adam
