056b81f32c46c700923582d869d27b9cf6bd931c
Vishal Singh <vsalsngh@gmail.com>
2018-01-08 10:28:04 +0530

    Sequential model

--- ANN-in-Keras/neural-nets.py
+++ ANN-in-Keras/neural-nets.py
@@ -4,3 +4,12 @@ from keras.models import Sequential
 from keras.layers import Activation
 from keras.layers.core import Dense
 from keras.optimizers import Adam
+from keras.metrices import categorical_crossentropy
+
+model = Sequential([
+  Dense(16, input_shape=(1,), activation='relu'),
+  Dense(32, activation='relu'),
+  Dense(2, activation='softmax')
+])
+
+model.summary()
