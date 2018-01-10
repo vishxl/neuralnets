28a316c2240f41c57a19cabeb64a3d3bba3c946a
Vishal Singh <vsalsngh@gmail.com>
2018-01-10 20:17:22 +0530

    Added Sample Preprocessing Data

--- ANN-in-Keras/neural-nets.py
+++ ANN-in-Keras/neural-nets.py
@@ -1,18 +1,73 @@
+import numpy as np
+from random import randint
+from sklearn.preprocessing import MinMaxScaler
+
+
+train_labels =  []
+train_samples = []
+
+
+for i in range(50):
+    # The 5% of younger individuals who did experience side effects
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(1)
+
+    # The 5% of older individuals who did not experience side effects
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(0)
+
+for i in range(1000):
+    # The 95% of younger individuals who did not experience side effects
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(0)
+
+    # The 95% of older individuals who did experience side effects
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(1)
+
+for i in train_samples:
+        print(i)
+
+
+
+
+for i in train_labels:
+    print(i)
+
+
+
+train_labels = np.array(train_labels)
+train_samples = np.array(train_samples)
+
+
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_train_samples = scaler.fit_transform((train_samples).reshape(-1,1))
+
+
+for i in scaled_train_samples:
+    print(i)
+
+
+
+#Sequential Model
+
 import keras
 from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Activation
 from keras.layers.core import Dense
 from keras.optimizers import Adam
-from keras.metrices import categorical_crossentropy
+from keras.metrics import categorical_crossentropy
 
 model = Sequential([
-  Dense(16, input_shape=(1,), activation='relu'),
-  Dense(32, activation='relu'),
-  Dense(2, activation='softmax')
+    Dense(16, input_shape=(1,), activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(2, activation='softmax')
 ])
 
 model.summary()
-
-model.compile(Adam (lr=.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(scaled_train_samples, train_lables, batch_size = 10, epochs = 20, suffle = True, verbose = 2 )
