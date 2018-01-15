1418933a0c808716637a6414697e67b6e0c01fe1
Vishal Singh <vsalsngh@gmail.com>
2018-01-16 00:03:00 +0530

    Update neural-nets.py

--- ANN-in-Keras/neural-nets.py
+++ ANN-in-Keras/neural-nets.py
@@ -100,4 +100,33 @@ model2 = Sequential([
 
 model2.load_weights('my_model_weights.h5')
 
+#Preprocess Test Data
+test_labels =  []
+test_samples = []
+
+for i in range(10):
+    # The 5% of younger individuals who did experience side effects
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(1)
+    
+    # The 5% of older individuals who did not experience side effects
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+
+for i in range(200):
+    # The 95% of younger individuals who did not experience side effects
+    random_younger = randint(13,64)
+    test_samples.append(random_younger)
+    test_labels.append(0)
+    
+    # The 95% of older individuals who did experience side effects
+    random_older = randint(65,100)
+    test_samples.append(random_older)
+    test_labels.append(1)
+    
+ test_labels = np.array(test_labels)
+test_samples = np.array(test_samples)
+
 
