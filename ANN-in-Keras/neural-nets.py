0d940a098d90365ef7f0a6b89a7ebf42949f8836
Vishal Singh <vsalsngh@gmail.com>
2018-01-22 23:08:44 +0530

    Update neural-nets.py

--- ANN-in-Keras/neural-nets.py
+++ ANN-in-Keras/neural-nets.py
@@ -168,4 +168,19 @@ def plot_confusion_matrix(cm, classes,
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
     
+      if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+        
+        
     
