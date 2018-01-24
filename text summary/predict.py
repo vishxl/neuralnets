190c1cfb5c6799dbe927722e1d6d00731737a1d4
Vishal Singh <vsalsngh@gmail.com>
2018-01-24 23:35:48 +0530

    Update predict.py

--- text summary/predict.py
+++ text summary/predict.py
@@ -15,3 +15,15 @@ rnn_size = 512
 rnn_layers = 3  # match FN1
 batch_norm=False
 
+activation_rnn_size = 40 if maxlend else 0
+
+# training parameters
+seed=42
+p_W, p_U, p_dense, p_emb, weight_decay = 0, 0, 0, 0, 0
+optimizer = 'adam'
+batch_size=64
+
+nb_train_samples = 30000
+nb_val_samples = 3000
+
+
