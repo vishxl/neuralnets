8365849f6ba988630687c5e87a2949ff2d9c55e6
Vishal Singh <vsalsngh@gmail.com>
2018-01-29 22:17:00 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -18,3 +18,13 @@ def visualize(X, y, model):
     plot_decision_boundary(lambda x:predict(model,x), X, y)
     plt.title("Logistic Regression")
 
+def plot_decision_boundary(pred_func, X, y):
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.show()
